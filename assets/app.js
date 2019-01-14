1aafeecc50e5bad76049345b9350987991829fe9
Zona <bryson.gilreath@gmail.com>
2019-01-13 18:33:00 -0600

    game logic mostly done, working on front-end

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,14 @@
-let needPlayer = true;
-let needDefender = true;
-
 class Hero {
-    constructor(name, health, base_attack, image_url) {
+    constructor(name, health, base_attack) {
         this.name = name;
         this.health = health;
         this.base_attack = base_attack;
         this.attack_power = base_attack;
-        this.isPlayer = false;
-        this.isDefending = false;
+        // this.isPlayer = false;
+        // this.isDefending = false;
         this.isDead = false;
         this.card = $("#" + this.name);
+        this.image = "assets/images/" + this.name + ".jpg";
     }
 
     power_up(){
@@ -30,28 +28,35 @@ class Hero {
 }
 
 
-const characters = {
-    goku: new Hero("Goku", 100, 10),
-    vegeta: new Hero("Vegeta", 120, 12),
-    trunks: new Hero("Trunks", 120, 12),
-    android18: new Hero("Android-18", 120, 12)
-}
-
-
 const game = {
+
+    characters : {
+        goku : new Hero("Goku", 100, 10),
+        vegeta : new Hero("Vegeta", 120, 12),
+        trunks : new Hero("Trunks", 120, 12),
+        android18 : new Hero("Android-18", 120, 12)
+    },
+
+    needPlayer : true,
+    needDefender: true,
 
     initGame : function(){
 
-        for (let character of characters){
+        game.needPlayer = true;
+        game.needDefender = true;
+
+        for (let character of game.characters){
             character.card.on("click", function(character){
                 if (game.needPlayer){
                     game.addPlayer(character);
+                    game.moveEnemies();
                 }else if (game.needDefender){
                     game.addDefender(character);
                 }
             });
         }
-        $("attackButton").on("click", function(){
+
+        $("#attackButton").on("click", function(){
             if (!game.needPlayer & !game.needDefender){
                 game.round();
             }
@@ -60,40 +65,67 @@ const game = {
 
     addPlayer : function(character){
         character.card.addClass("player");
-        character.isPlayer = true;
+        // character.card.removeClass("in-lobby");
+        $("#lobby").remove(character.card);
+        $("#player-area").append(character.card);
         game.player = character;
+    },
+
+    moveEnemies : function(){
+        for (let character of game.characters){
+            if (game.player != character){
+                character.card.addClass("enemy");
+                $("#lobby").remove(character.card);
+                // character.card.removeClass("in-lobby");
+                $("#enemy-area").append(character.card);
+            }
+        }
     },
 
     addDefender : function(character){
         this.addClass("defender");
-        character.isDefending = true;
         game.defender = character;
+        $("#lobby").remove(character.card);
+        // character.card.removeClass("in-lobby");
+        $("#enemyArea").append(character.card);
     },
 
-    round : function(player, defender){
-        player.attack(defender);
-        player.power_up();
-        defender.attack(player);
+    round : function(){
+        game.player.attack(defender);
+        game.player.power_up();
+        game.defender.attack(player);
         game.checkBattleEnd();
     },
 
-    checkBattleEnd : function(player, defender) {
-        if (defender.checkDead()){
-            game.battleWin();
+    checkBattleEnd : function() {
+        if (game.defender.checkDead()){
+            game.winBattle();
         }
-        if (player.checkDead()) {
-            game.lose();
+        if (game.player.checkDead()) {
+            game.loseGame();
         }
     },
 
-    battleWin : function(){
+    winBattle : function(){
         game.defender.card.remove();
         game.defender = none;
         needDefender = true;
     },
 
-    lose : function(){
+    loseGame : function(){
 
+    },
+
+    winGame : function(){
+
+    },
+
+    resetGame : function(){
+        for (let character of game.characters){
+            character.card.off("click");
+        }
+
+        $("#attackButton").off("click");
     }
 
 }
@@ -101,5 +133,3 @@ const game = {
 
 goku = new Hero("Goku", 100, 10);
 vegeta = new Hero("Vegeta", 120, 12);
-
-console.log(a);
